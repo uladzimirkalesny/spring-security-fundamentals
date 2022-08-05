a41915c120b7c81eac08203b8cdc5a739acc9e2f
Uladzimir Kalesny <uladzimirkalesny@gmail.com>
2022-08-05 20:05:05 +0300

    [spring-security-0] - Initial Commit

---
+++ src/main/java/com/example/springsecurityfundamentals/SpringSecurityFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFundamentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityFundamentalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecurityfundamentals/SpringSecurityFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFundamentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
